f4d876062800125743b325ea06c11ca7fc5b814a
ASWINKUMAR SINGARAM NATARAJAN <SiNat@ASWINKUMARs-iMac.local>
2020-04-30 09:31:21 +1000

    changed back to shorter keyword

--- app.py
+++ app.py
@@ -14,14 +14,7 @@ auth.set_access_token(a_token, a_t_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-key_words = (
-    "FAIR research data management principles"
-    or "FAIR data"
-    or "#DATAMANAGEMENT"
-    or "#datamanagement"
-    or "good practice data management"
-    or "#FAIRdata"
-)
+key_words = "FAIR data management"
 
 nrTweets = 800
 
