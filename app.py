548ba6fbc591fd033fef8e7fc2b0e49c68ae5c0c
ASWINKUMAR SINGARAM NATARAJAN <SiNat@ASWINKUMARs-iMac.local>
2020-04-30 09:44:35 +1000

    3 keywords

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ auth.set_access_token(a_token, a_t_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-key_words = "FAIRdata" or "datamanagement"
+key_words = "FAIRdata" or "datamanagement" or "researchdata"
 
 nrTweets = 800
 
