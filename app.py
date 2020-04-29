e950ba3bb9944dcee4b2f0dcc02ac229b6534599
ASWINKUMAR SINGARAM NATARAJAN <SiNat@ASWINKUMARs-iMac.local>
2020-04-30 09:49:55 +1000

    added opensci

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ auth.set_access_token(a_token, a_t_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
-key_words = "FAIRdata" or "datamanagement" or "researchdata"
+key_words = "FAIRdata" or "datamanagement" or "researchdata" or "OpenScience"
 
 nrTweets = 800
 
